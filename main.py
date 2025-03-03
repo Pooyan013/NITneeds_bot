0c9acbe78d3827eb1182e50d9c0c0b4adf8ff9a9
Hossein Ahmadi <Hopoah1221@gmail.com>
2025-03-04 00:24:38 +0330

    Add Reply and Timer

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import telebot
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
+import time
 from keys import *
-
-hash ="7926804782:AAEwdUS3bZB0Dvit8Y5NRa8mNaaiCpZQMFM"
 
 bot = telebot.TeleBot(hash)
 
+#___________________________________BUTTONS________________________________________________
 buttons = ["📎 درخواستی", "🏷 فروشی", "❓پرسش", "🔍 گمشده / پیدا شده", "📚فایل‌های درسی", "📩 اطلاعات اساتید", "📈 تبلیغات", "📞 ارتباط با ادمین"]
 keyboard_markup = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
 keyboard_markup.add(*buttons)
@@ -15,6 +15,8 @@ faculty_markup = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
 faculty_markup.add(*faculty)
 
 professor = []
+
+#____________________________________HANDELERS_____________________________________________
 
 @bot.message_handler(commands=["admin"])
 def admin(message):
@@ -29,19 +31,73 @@ def send_welcome(message):
 @bot.message_handler()
 def main(message):
     if message.text == "📎 درخواستی":
-        bot.send_message(message.chat.id, "درخواستی")
+        bot.send_message(message.chat.id, text_darkhasti)
+        if message.text == "📎 درخواستی":
+            time.sleep(120)
+            bot.send_message(message.chat.id, """زمان شما برای ارسال پیام تمام شد لطفا از قبل پیام خود را آماده و سپس درخواست دهید
+برای شروع مجدد روی /start کلیک کنید""")
+        else:
+            user_message = message.text  
+            modified_message = f"""#درخواستی
+            {user_message}"""
+            bot.reply_to(message, modified_message)
+            bot.send_message(message.chat.id, '''در خواست شما با موفقیت برای ادمین ارسال شد، درصورت تایید درکانال و درغیر این صورت دلیل رد شدن به شما اطلاع داده میشود
+                            
+درصورت مشاهده هرگونه مشکل ممنون میشم به ایدی @Pooyan013 پیام دهید ''')
+
     elif message.text == "🏷 فروشی":
-        bot.send_message(message.chat.id, "فروشی")
+        bot.send_message(message.chat.id, text_foroshi)
+        time.sleep(120)
+        if message.text == "🏷 فروشی":
+            bot.send_message(message.chat.id, """زمان شما برای ارسال پیام تمام شد لطفا از قبل پیام خود را آماده و سپس درخواست دهید
+برای شروع مجدد روی /start کلیک کنید""")  
+        else:
+            user_message = message.text  
+            modified_message = f"""#فروشی
+            {user_message}"""
+            bot.reply_to(message, modified_message)
+            bot.send_message(message.chat.id, '''در خواست شما با موفقیت برای ادمین ارسال شد، درصورت تایید درکانال و درغیر این صورت دلیل رد شدن به شما اطلاع داده میشود
+                         
+درصورت مشاهده هرگونه مشکل ممنون میشم به ایدی @Pooyan013 پیام دهید ''')
+
     elif message.text == "❓پرسش":
-        bot.send_message(message.chat.id, "پرسش")
+        bot.send_message(message.chat.id, text_porsesh)
+        time.sleep(120)
+        if message.text == "❓پرسش":
+            bot.send_message(message.chat.id, """زمان شما برای ارسال پیام تمام شد لطفا از قبل پیام خود را آماده و سپس درخواست دهید
+برای شروع مجدد روی /start کلیک کنید""")  
+        else:
+            user_message = message.text  
+            modified_message = f"""#پرسش
+            {user_message}"""
+            bot.reply_to(message, modified_message)
+            bot.send_message(message.chat.id, '''در خواست شما با موفقیت برای ادمین ارسال شد، درصورت تایید درکانال و درغیر این صورت دلیل رد شدن به شما اطلاع داده میشود
+                            
+    درصورت مشاهده هرگونه مشکل ممنون میشم به ایدی @Pooyan013 پیام دهید ''')
     elif message.text == "🔍 گمشده / پیدا شده":
-        bot.send_message(message.chat.id, "گمشده / پیدا شده")
+        bot.send_message(message.chat.id, text_gomshode)
+        time.sleep(120)
+        if message.text == "🔍 گمشده / پیدا شده":
+            bot.send_message(message.chat.id, """زمان شما برای ارسال پیام تمام شد لطفا از قبل پیام خود را آماده و سپس درخواست دهید
+برای شروع مجدد روی /start کلیک کنید""")
+        else:  
+            user_message = message.text  
+            modified_message = f"""#درخواستی
+            {user_message}"""
+            bot.reply_to(message, modified_message)
+            bot.send_message(message.chat.id, '''در خواست شما با موفقیت برای ادمین ارسال شد، درصورت تایید درکانال و درغیر این صورت دلیل رد شدن به شما اطلاع داده میشود
+                            
+    درصورت مشاهده هرگونه مشکل ممنون میشم به ایدی @Pooyan013 پیام دهید ''')
+
     elif message.text == "📚فایل‌های درسی":
         bot.send_message(message.chat.id, "فایل‌های درسی")
+
     elif message.text == "📩 اطلاعات اساتید":
         bot.send_message(message.chat.id, "در مورد استاد کدوم دانشکده میخوای اطلاعات بدم؟",reply_markup=faculty_markup)
+
     elif message.text == "📈 تبلیغات":
         bot.send_message(message.chat.id, text_tablighat)
+
     elif message.text == "📞 ارتباط با ادمین":
         bot.send_message(message.chat.id, text_admin)
 bot.infinity_polling()
