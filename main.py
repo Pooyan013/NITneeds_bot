b411bd69ac3af88d80ad3bb374c714a2958a0156
Hossein Ahmadi <Hopoah1221@gmail.com>
2025-03-03 22:51:47 +0330

    First Session Debugged

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
+from keys import *
 
 hash ="7926804782:AAEwdUS3bZB0Dvit8Y5NRa8mNaaiCpZQMFM"
 
@@ -9,6 +10,12 @@ buttons = ["📎 درخواستی", "🏷 فروشی", "❓پرسش", "🔍 گم
 keyboard_markup = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
 keyboard_markup.add(*buttons)
 
+faculty = ["علوم پایه", "مکانیک" , "عمران", "شیمی", "صنایع و مواد", "برق و کامپیوتر" ]
+faculty_markup = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+faculty_markup.add(*faculty)
+
+professor = []
+
 @bot.message_handler(commands=["admin"])
 def admin(message):
     if message.from_user.id in [112911597, 101108999]:
@@ -16,7 +23,25 @@ def admin(message):
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-    bot.send_message(message.chat.id, f"سلام به ربات نیازمندی ها خوش اومدی چجوری میتونم بهت کمک کنم؟", reply_markup=keyboard_markup)
+    bot.send_message(message.chat.id, f"""سلام به ربات نیازمندی‌ها خوش اومدی 🩷
+چطوری میتونم بهت کمک کنم؟""", reply_markup=keyboard_markup)
 
-
+@bot.message_handler()
+def main(message):
+    if message.text == "📎 درخواستی":
+        bot.send_message(message.chat.id, "درخواستی")
+    elif message.text == "🏷 فروشی":
+        bot.send_message(message.chat.id, "فروشی")
+    elif message.text == "❓پرسش":
+        bot.send_message(message.chat.id, "پرسش")
+    elif message.text == "🔍 گمشده / پیدا شده":
+        bot.send_message(message.chat.id, "گمشده / پیدا شده")
+    elif message.text == "📚فایل‌های درسی":
+        bot.send_message(message.chat.id, "فایل‌های درسی")
+    elif message.text == "📩 اطلاعات اساتید":
+        bot.send_message(message.chat.id, "در مورد استاد کدوم دانشکده میخوای اطلاعات بدم؟",reply_markup=faculty_markup)
+    elif message.text == "📈 تبلیغات":
+        bot.send_message(message.chat.id, text_tablighat)
+    elif message.text == "📞 ارتباط با ادمین":
+        bot.send_message(message.chat.id, text_admin)
 bot.infinity_polling()
